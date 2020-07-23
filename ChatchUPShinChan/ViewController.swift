a4076440db86f3845fcb5519fbefd9bfa9e95668
sarath <sarathatit@gmail.com>
2020-07-23 20:27:50 +0530

    Initial Commit

---
+++ ChatchUPShinChan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatchUPShinChan
+//
+//  Created by sarath kumar on 23/07/20.
+//  Copyright © 2020 sarath kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
